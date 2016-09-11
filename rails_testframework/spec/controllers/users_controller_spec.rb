a9468447a625a033fa252a004e0fa148ad60e427
Calvin <Calvin.lacc@gmail.com>
2016-09-11 15:09:56 -0600

    Testing the controller using examples

--- rails_testframework/spec/controllers/users_controller_spec.rb
+++ rails_testframework/spec/controllers/users_controller_spec.rb
@@ -2,4 +2,21 @@ require 'rails_helper'
 
 RSpec.describe UsersController, :type => :controller do
 
+  describe "show" do
+    let(:user){build(:user)}
+    
+    before do
+      user.save
+    end
+
+    it "sets the correct instance variable" do
+      get :show, :id => user.id
+    end
+
+    # it "has the title of the correct user" do
+    #   user.save
+    #   visit user_path(user)
+
+  end
+
 end
